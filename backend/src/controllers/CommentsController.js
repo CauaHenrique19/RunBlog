2b237bc125a4a7a553e986f7ff0986b8c2d0217d
Cauã Henrique <cauah6002@gmail.com>
2020-11-24 14:54:12 -0300

    Preparando backend para retornar comentário ao usuário comentar em um post

--- backend/src/controllers/CommentsController.js
+++ backend/src/controllers/CommentsController.js
@@ -1,24 +1,25 @@
 const knex = require('../database/connection')
+const formatDataAndHour = require('../utils/FormatDataAndHour')
 
 class Comments{
     async index(req, res){
         const comments = await knex('comments')
             .select(
-                'comments.id', 'comments.userId', 'users.email as emailUser', 
+                'comments.id', 'comments.userId', 'users.email as userEmail', 'users.name as userName', 
                 'comments.articleId', 'articles.title as titleArticle', 'articles.imageUrl', 
                 'comments.content', 'comments.createdAt')
             .join('users', 'comments.userId', '=', 'users.id')
             .join('articles', 'comments.articleId', '=', 'articles.id')
             .orderBy('id')
         
-        comments.map((comment) => comment.createdAt = comment.createdAt.toLocaleString())
+        comments.map((comment) => comment.createdAt = formatDataAndHour(comment.createdAt))
 
         res.json(comments)
     }
     create(req, res){
         const { userId, articleId, content } = req.body
 
-        const like = {
+        const coment = {
             userId,
             articleId,
             content,
@@ -26,15 +27,19 @@ class Comments{
         }
 
         knex('comments')
-            .insert(like)
-            .then(result => res.json({ message: 'Comentário realizado com sucesso!' }))
+            .insert(coment, '*')
+            .then(returnedComent => {
+                returnedComent[0].createdAt = formatDataAndHour(returnedComent[0].createdAt)
+                delete returnedComent[0].articleId
+                res.json({ message: 'Comentário realizado com sucesso!', coment: returnedComent[0] })
+            })
             .catch(error => console.log(error.message))
     }
     delete(req, res){
         knex('comments')
             .where('id', req.params.id)
             .delete()
-        .then(result => res.json({ message: 'Comentário excluído com sucesso!' }))
+        .then(() => res.json({ message: 'Comentário excluído com sucesso!' }))
         .catch(error => console.log(error))
     }
 }
