287e0cc7af857567e92aec84c91859303b7e533d
Cauã Henrique <cauah6002@gmail.com>
2020-11-24 15:54:53 -0300

    Adicionando funcionalidade de pesquisar na sidebar

--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Context } from '../../context/context'
 
 import './Sidebar.css'
@@ -6,18 +6,50 @@ import './Sidebar.css'
 const SideBar = () => {
 
     const { article, articles, setViewContent, setViewForm, setViewNewArticles, setArticle } = useContext(Context)
+    const [searchValue, setSearchValue] = useState('')
+    const [renderArticles, setRenderArticles] = useState([])
+
+    useEffect(() => {
+        setRenderArticles(articles)
+    }, [articles])
+
+    function handleSearch(e){
+        const searchedArticles = articles.filter(articleFind => articleFind.title.toLowerCase().includes(e.target.value.toLowerCase()))
+        setSearchValue(e.target.value.toLowerCase())
+        setRenderArticles(searchedArticles)
+    }
 
     return (
         <div className="sidebar">
             <div className="search">
-                <input type="text" name="search" id="search" placeholder="Pesquisar" />
+                <input type="text" 
+                    name="search" 
+                    id="search" 
+                    placeholder="Pesquisar" 
+                    value={searchValue} 
+                    onChange={handleSearch} 
+                />
                 <ion-icon name="search-outline"></ion-icon>
             </div>
             <h1>Artigos</h1>
+            {
+                searchValue && 
+                <div className="search-value">
+                    <p>{searchValue}</p>
+                    <ion-icon 
+                        title="Limpar Pesquisa"
+                        name="close-outline" 
+                        onClick={() => {
+                            setSearchValue('') 
+                            setRenderArticles(articles)
+                        }}>
+                    </ion-icon>
+                </div>
+            }
             <ul>
                 {   
-                    articles.length > 0 &&
-                    articles.map(articleRender => (
+                    renderArticles.length > 0 ?
+                    renderArticles.map(articleRender => (
                         <li className={ article && article.id === articleRender.id ? 'selected' : '' } key={articleRender.id}
                             onClick={() => {
                                 setViewContent(true)
@@ -28,7 +60,11 @@ const SideBar = () => {
                             <img src={articleRender.imageUrl} alt={articleRender.title} />
                             <p>{articleRender.title}</p>
                         </li>
-                    ))
+                    )) :
+                    <div className="warning">
+                        <ion-icon name="warning-outline"></ion-icon>
+                        <h2>Nenhum artigo encontrado!</h2>
+                    </div>
                 }
             </ul>
         </div>)
