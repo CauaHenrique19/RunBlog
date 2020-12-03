5e409e5304fa2435e726ae6beda0f43548616d1f
Cauã Henrique <cauah6002@gmail.com>
2020-12-03 15:34:09 -0300

    Construindo componente PageNotFound

--- frontend/src/pages/PageNotFound/index.js
+++ frontend/src/pages/PageNotFound/index.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './style.css'
+import image from '../../assets/404.jpg'
 
 const PageNotFound = () => {
     return (
-        <h1>Página Não Encontrada</h1>
+        <div className="not-found" >
+            <h1>Página Não Encontrada!</h1>
+            <img src={image} alt=""/>
+            <Link to="/" >Voltar Para Home</Link>
+        </div>
     )
 }
 
