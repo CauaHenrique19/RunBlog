f5eccc8ad389ed4b3ef5045196b8c33be3c465c9
Cauã Henrique <cauah6002@gmail.com>
2020-12-03 14:09:34 -0300

    Removendo socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const app = express()
-const http = require('http').createServer(app)
-const io = require('socket.io')(http)
 const cors = require('cors')
 const routes = require('./routes')
 
@@ -9,7 +7,4 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-io.on('connection', (socket) => console.log('Novo Cliente Conectado!'))
-http.listen(3001, () => console.log('[BACK] Rodando Backend'))
-
-module.exports = io
+app.listen(3001, () => console.log('[BACK] Rodando Backend'))
